0f06d3d2ba12fa0e0c2d931d664f9cbc1967cebb
FR33THY <fr33thy@gmail.com>
2025-03-16 16:28:50 +1100

    Add files via upload

---
+++ 6 Windows/6 Gamebar.ps1
@@ -0,0 +1,185 @@
+    If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
+    {Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit}
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + " (Administrator)"
+    $Host.UI.RawUI.BackgroundColor = "Black"
+	$Host.PrivateData.ProgressBackgroundColor = "Black"
+    $Host.PrivateData.ProgressForegroundColor = "White"
+    Clear-Host
+
+    function Get-FileFromWeb {
+    param ([Parameter(Mandatory)][string]$URL, [Parameter(Mandatory)][string]$File)
+    function Show-Progress {
+    param ([Parameter(Mandatory)][Single]$TotalValue, [Parameter(Mandatory)][Single]$CurrentValue, [Parameter(Mandatory)][string]$ProgressText, [Parameter()][int]$BarSize = 10, [Parameter()][switch]$Complete)
+    $percent = $CurrentValue / $TotalValue
+    $percentComplete = $percent * 100
+    if ($psISE) { Write-Progress "$ProgressText" -id 0 -percentComplete $percentComplete }
+    else { Write-Host -NoNewLine "`r$ProgressText $(''.PadRight($BarSize * $percent, [char]9608).PadRight($BarSize, [char]9617)) $($percentComplete.ToString('##0.00').PadLeft(6)) % " }
+    }
+    try {
+    $request = [System.Net.HttpWebRequest]::Create($URL)
+    $response = $request.GetResponse()
+    if ($response.StatusCode -eq 401 -or $response.StatusCode -eq 403 -or $response.StatusCode -eq 404) { throw "Remote file either doesn't exist, is unauthorized, or is forbidden for '$URL'." }
+    if ($File -match '^\.\\') { $File = Join-Path (Get-Location -PSProvider 'FileSystem') ($File -Split '^\.')[1] }
+    if ($File -and !(Split-Path $File)) { $File = Join-Path (Get-Location -PSProvider 'FileSystem') $File }
+    if ($File) { $fileDirectory = $([System.IO.Path]::GetDirectoryName($File)); if (!(Test-Path($fileDirectory))) { [System.IO.Directory]::CreateDirectory($fileDirectory) | Out-Null } }
+    [long]$fullSize = $response.ContentLength
+    [byte[]]$buffer = new-object byte[] 1048576
+    [long]$total = [long]$count = 0
+    $reader = $response.GetResponseStream()
+    $writer = new-object System.IO.FileStream $File, 'Create'
+    do {
+    $count = $reader.Read($buffer, 0, $buffer.Length)
+    $writer.Write($buffer, 0, $count)
+    $total += $count
+    if ($fullSize -gt 0) { Show-Progress -TotalValue $fullSize -CurrentValue $total -ProgressText " $($File.Name)" }
+    } while ($count -gt 0)
+    }
+    finally {
+    $reader.Close()
+    $writer.Close()
+    }
+    }
+
+    Write-Host "1. Gamebar Xbox: Off (Recommended)"
+    Write-Host "2. Gamebar Xbox: Default"
+    while ($true) {
+    $choice = Read-Host " "
+    if ($choice -match '^[1-2]$') {
+    switch ($choice) {
+    1 {
+
+Clear-Host
+$progresspreference = 'silentlycontinue'
+# disable gamebar regedit
+reg add "HKCU\System\GameConfigStore" /v "GameDVR_Enabled" /t REG_DWORD /d "0" /f | Out-Null
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v "AppCaptureEnabled" /t REG_DWORD /d "0" /f | Out-Null
+# disable open xbox game bar using game controller regedit
+reg add "HKCU\Software\Microsoft\GameBar" /v "UseNexusForGameBarEnabled" /t REG_DWORD /d "0" /f | Out-Null
+# disable gameinput service regedit
+reg add "HKLM\SYSTEM\ControlSet001\Services\GameInputSvc" /v "Start" /t REG_DWORD /d "4" /f | Out-Null
+# disable gamedvr and broadcast user service regedit
+reg add "HKLM\SYSTEM\ControlSet001\Services\BcastDVRUserService" /v "Start" /t REG_DWORD /d "4" /f | Out-Null
+# disable xbox accessory management service regedit
+reg add "HKLM\SYSTEM\ControlSet001\Services\XboxGipSvc" /v "Start" /t REG_DWORD /d "4" /f | Out-Null
+# disable xbox live auth manager service regedit
+reg add "HKLM\SYSTEM\ControlSet001\Services\XblAuthManager" /v "Start" /t REG_DWORD /d "4" /f | Out-Null
+# disable xbox live game save service regedit
+reg add "HKLM\SYSTEM\ControlSet001\Services\XblGameSave" /v "Start" /t REG_DWORD /d "4" /f | Out-Null
+# disable xbox live networking service regedit
+reg add "HKLM\SYSTEM\ControlSet001\Services\XboxNetApiSvc" /v "Start" /t REG_DWORD /d "4" /f | Out-Null
+# disable ms-gamebar notifications with xbox controller plugged in regedit
+# create reg file
+$MultilineComment = @"
+Windows Registry Editor Version 5.00
+
+; disable ms-gamebar notifications with xbox controller plugged in
+[HKEY_CLASSES_ROOT\ms-gamebar]
+"URL Protocol"=""
+"NoOpenWith"=""
+@="URL:ms-gamebar"
+
+[HKEY_CLASSES_ROOT\ms-gamebar\shell\open\command]
+@="\"%SystemRoot%\\System32\\systray.exe\""
+
+[HKEY_CLASSES_ROOT\ms-gamebarservices]
+"URL Protocol"=""
+"NoOpenWith"=""
+@="URL:ms-gamebarservices"
+
+[HKEY_CLASSES_ROOT\ms-gamebarservices\shell\open\command]
+@="\"%SystemRoot%\\System32\\systray.exe\""
+
+[HKEY_CLASSES_ROOT\ms-gamingoverlay]
+"URL Protocol"=""
+"NoOpenWith"=""
+@="URL:ms-gamingoverlay"
+
+[HKEY_CLASSES_ROOT\ms-gamingoverlay\shell\open\command]
+@="\"%SystemRoot%\\System32\\systray.exe\""
+"@
+Set-Content -Path "$env:TEMP\MsGamebarNotiOff.reg" -Value $MultilineComment -Force
+# import reg file
+Regedit.exe /S "$env:TEMP\MsGamebarNotiOff.reg"
+# stop gamebar running
+Stop-Process -Force -Name GameBar -ErrorAction SilentlyContinue | Out-Null
+# uninstall gamebar & xbox apps
+Get-AppxPackage -allusers *Microsoft.GamingApp* | Remove-AppxPackage
+Get-AppxPackage -allusers *Microsoft.Xbox.TCUI* | Remove-AppxPackage
+Get-AppxPackage -allusers *Microsoft.XboxApp* | Remove-AppxPackage
+Get-AppxPackage -allusers *Microsoft.XboxGameOverlay* | Remove-AppxPackage
+Get-AppxPackage -allusers *Microsoft.XboxGamingOverlay* | Remove-AppxPackage
+Get-AppxPackage -allusers *Microsoft.XboxIdentityProvider* | Remove-AppxPackage
+Get-AppxPackage -allusers *Microsoft.XboxSpeechToTextOverlay* | Remove-AppxPackage
+Write-Host "Restart to apply . . ."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Start-Process ms-settings:gaming-gamebar
+exit
+
+      }
+    2 {
+
+Clear-Host
+$progresspreference = 'silentlycontinue'
+# gamebar regedit
+reg add "HKCU\System\GameConfigStore" /v "GameDVR_Enabled" /t REG_DWORD /d "1" /f | Out-Null
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v "AppCaptureEnabled" /t REG_DWORD /d "1" /f | Out-Null
+# open xbox game bar using game controller regedit
+cmd.exe /c "reg delete `"HKCU\Software\Microsoft\GameBar`" /v `"UseNexusForGameBarEnabled`" /f >nul 2>&1"
+# gameinput service
+reg add "HKLM\SYSTEM\ControlSet001\Services\GameInputSvc" /v "Start" /t REG_DWORD /d "3" /f | Out-Null
+# gamedvr and broadcast user service regedit
+reg add "HKLM\SYSTEM\ControlSet001\Services\BcastDVRUserService" /v "Start" /t REG_DWORD /d "3" /f | Out-Null
+# xbox accessory management service regedit
+reg add "HKLM\SYSTEM\ControlSet001\Services\XboxGipSvc" /v "Start" /t REG_DWORD /d "3" /f | Out-Null
+# xbox live auth manager service regedit
+reg add "HKLM\SYSTEM\ControlSet001\Services\XblAuthManager" /v "Start" /t REG_DWORD /d "3" /f | Out-Null
+# xbox live game save service regedit
+reg add "HKLM\SYSTEM\ControlSet001\Services\XblGameSave" /v "Start" /t REG_DWORD /d "3" /f | Out-Null
+# xbox live networking service regedit
+reg add "HKLM\SYSTEM\ControlSet001\Services\XboxNetApiSvc" /v "Start" /t REG_DWORD /d "3" /f | Out-Null
+# ms-gamebar notifications with xbox controller plugged in regedit
+# create reg file
+$MultilineComment = @"
+Windows Registry Editor Version 5.00
+
+; ms-gamebar notifications with xbox controller plugged in regedit
+[-HKEY_CLASSES_ROOT\ms-gamebar]
+[-HKEY_CLASSES_ROOT\ms-gamebarservices]
+[-HKEY_CLASSES_ROOT\ms-gamingoverlay\shell]
+
+[HKEY_CLASSES_ROOT\ms-gamingoverlay]
+"URL Protocol"=""
+@="URL:ms-gamingoverlay"
+"@
+Set-Content -Path "$env:TEMP\MsGamebarNotiOn.reg" -Value $MultilineComment -Force
+# import reg file
+Regedit.exe /S "$env:TEMP\MsGamebarNotiOn.reg"
+# install store, gamebar & xbox apps
+Get-AppXPackage -AllUsers *Microsoft.GamingApp* | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register -ErrorAction SilentlyContinue "$($_.InstallLocation)\AppXManifest.xml"}
+Get-AppXPackage -AllUsers *Microsoft.Xbox.TCUI* | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register -ErrorAction SilentlyContinue "$($_.InstallLocation)\AppXManifest.xml"}
+Get-AppXPackage -AllUsers *Microsoft.XboxApp* | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register -ErrorAction SilentlyContinue "$($_.InstallLocation)\AppXManifest.xml"}
+Get-AppXPackage -AllUsers *Microsoft.XboxGameOverlay* | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register -ErrorAction SilentlyContinue "$($_.InstallLocation)\AppXManifest.xml"}
+Get-AppXPackage -AllUsers *Microsoft.XboxGamingOverlay* | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register -ErrorAction SilentlyContinue "$($_.InstallLocation)\AppXManifest.xml"}
+Get-AppXPackage -AllUsers *Microsoft.XboxIdentityProvider* | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register -ErrorAction SilentlyContinue "$($_.InstallLocation)\AppXManifest.xml"}
+Get-AppXPackage -AllUsers *Microsoft.XboxSpeechToTextOverlay* | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register -ErrorAction SilentlyContinue "$($_.InstallLocation)\AppXManifest.xml"}
+Get-AppXPackage -AllUsers *Microsoft.WindowsStore* | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register -ErrorAction SilentlyContinue "$($_.InstallLocation)\AppXManifest.xml"}
+Get-AppXPackage -AllUsers *Microsoft.Microsoft.StorePurchaseApp * | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register -ErrorAction SilentlyContinue "$($_.InstallLocation)\AppXManifest.xml"}
+Write-Host "Installing: Edge Webview . . ."
+# download edge webview installer
+Get-FileFromWeb -URL "https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/304fddef-b073-4e0a-b1ff-c2ea02584017/MicrosoftEdgeWebview2Setup.exe" -File "$env:TEMP\EdgeWebView.exe"
+Clear-Host
+# start edge webview installer
+Start-Process -wait "$env:TEMP\EdgeWebView.exe"
+Clear-Host
+# download gamebar repair tool
+Get-FileFromWeb -URL "https://aka.ms/GamingRepairTool" -File "$env:TEMP\GamingRepairTool.exe"
+# start gamebar repair too
+Start-Process -wait "$env:TEMP\GamingRepairTool.exe"
+Clear-Host
+Write-Host "Restart to apply . . ."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+exit
+
+      }
+    } } else { Write-Host "Invalid input. Please select a valid option (1-2)." } }
