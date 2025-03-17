640044aa5a16c742da69d75c8420e6dde6ff9483
FR33THY <fr33thy@gmail.com>
2025-03-18 09:39:22 +1100

    Add files via upload

---
+++ 8 Advanced/4 ULPS AMD.ps1
@@ -0,0 +1,67 @@
+    If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
+    {Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit}
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + " (Administrator)"
+    $Host.UI.RawUI.BackgroundColor = "Black"
+	$Host.PrivateData.ProgressBackgroundColor = "Black"
+    $Host.PrivateData.ProgressForegroundColor = "White"
+    Clear-Host
+
+    Write-Host "AMD ULPS Ultra Low Power Saving"
+    Write-Host "1. Disable"
+    Write-Host "2. On (Default)"
+    while ($true) {
+    $choice = Read-Host " "
+    if ($choice -match '^[1-2]$') {
+    switch ($choice) {
+    1 {
+
+Clear-Host
+# get gpu driver id
+$subkeys = (Get-ChildItem -Path "Registry::HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}" -Force -ErrorAction SilentlyContinue).Name
+foreach($key in $subkeys){
+if ($key -notlike '*Configuration'){
+# disable ulps
+reg add "$key" /v "EnableUlps" /t REG_DWORD /d "0" /f | Out-Null
+}
+}
+Clear-Host
+Write-Host "ULPS: Disabled . . ."
+$subkeys = (Get-ChildItem -Path "Registry::HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}" -Force -ErrorAction SilentlyContinue).Name
+foreach($key in $subkeys){
+if ($key -notlike '*Configuration'){
+# show regedit value
+Get-ItemProperty -Path "Registry::$key" -Name 'EnableUlps'
+}
+}
+Write-Host "Restart to apply . . ."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+exit
+
+      }
+    2 {
+
+Clear-Host
+# get gpu driver id
+$subkeys = (Get-ChildItem -Path "Registry::HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}" -Force -ErrorAction SilentlyContinue).Name
+foreach($key in $subkeys){
+if ($key -notlike '*Configuration'){
+reg add "$key" /v "EnableUlps" /t REG_DWORD /d "1" /f | Out-Null
+# enable ulps
+}
+}
+Clear-Host
+Write-Host "ULPS: On . . ."
+$subkeys = (Get-ChildItem -Path "Registry::HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}" -Force -ErrorAction SilentlyContinue).Name
+foreach($key in $subkeys){
+if ($key -notlike '*Configuration'){
+# show regedit value
+Get-ItemProperty -Path "Registry::$key" -Name 'EnableUlps'
+}
+}
+Write-Host "Restart to apply . . ."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+exit
+
+      }
+    } } else { Write-Host "Invalid input. Please select a valid option (1-2)." } }
